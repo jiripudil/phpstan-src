3aa3e6426a0e03da79bbb047cfd658440582934a
Jiří Pudil <me@jiripudil.cz>
2023-06-18 22:43:02 +0200

    type projections: enforce elemental rules

--- src/Rules/Generics/GenericObjectTypeCheck.php
+++ src/Rules/Generics/GenericObjectTypeCheck.php
@@ -8,6 +8,7 @@ use PHPStan\ShouldNotHappenException;
 use PHPStan\Type\Generic\GenericObjectType;
 use PHPStan\Type\Generic\TemplateType;
 use PHPStan\Type\Generic\TemplateTypeHelper;
+use PHPStan\Type\Generic\TypeProjectionHelper;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeTraverser;
 use PHPStan\Type\VerbosityLevel;
@@ -30,6 +31,8 @@ class GenericObjectTypeCheck
 		string $notEnoughTypesMessage,
 		string $extraTypesMessage,
 		string $typeIsNotSubtypeMessage,
+		string $typeProjectionHasConflictingVarianceMessage,
+		string $typeProjectionIsRedundantMessage,
 	): array
 	{
 		$genericTypes = $this->getGenericTypes($phpDocType);
@@ -51,6 +54,7 @@ class GenericObjectTypeCheck
 			$templateTypes = array_values($classReflection->getTemplateTypeMap()->getTypes());
 
 			$genericTypeTypes = $genericType->getTypes();
+			$genericTypeVariances = $genericType->getVariances();
 			$templateTypesCount = count($templateTypes);
 			$genericTypeTypesCount = count($genericTypeTypes);
 			if ($templateTypesCount > $genericTypeTypesCount) {
@@ -80,8 +84,32 @@ class GenericObjectTypeCheck
 				}
 
 				$templateType = $templateTypes[$i];
-				$boundType = TemplateTypeHelper::resolveToBounds($templateType);
 				$genericTypeType = $genericTypeTypes[$i];
+
+				$genericTypeVariance = $genericTypeVariances[$i];
+				if ($templateType instanceof TemplateType && !$genericTypeVariance->invariant()) {
+					if ($genericTypeVariance->equals($templateType->getVariance())) {
+						$messages[] = RuleErrorBuilder::message(sprintf(
+							$typeProjectionIsRedundantMessage,
+							TypeProjectionHelper::describe($genericTypeType, $genericTypeVariance, VerbosityLevel::typeOnly()),
+							$genericType->describe(VerbosityLevel::typeOnly()),
+							$templateType->describe(VerbosityLevel::typeOnly()),
+							$classLikeDescription,
+							$classReflection->getDisplayName(false),
+						))->build();
+					} elseif (!$genericTypeVariance->validPosition($templateType->getVariance())) {
+						$messages[] = RuleErrorBuilder::message(sprintf(
+							$typeProjectionHasConflictingVarianceMessage,
+							TypeProjectionHelper::describe($genericTypeType, $genericTypeVariance, VerbosityLevel::typeOnly()),
+							$genericType->describe(VerbosityLevel::typeOnly()),
+							$templateType->describe(VerbosityLevel::typeOnly()),
+							$classLikeDescription,
+							$classReflection->getDisplayName(false),
+						))->build();
+					}
+				}
+
+				$boundType = TemplateTypeHelper::resolveToBounds($templateType);
 				if ($boundType->isSuperTypeOf($genericTypeType)->yes()) {
 					if (!$templateType instanceof TemplateType) {
 						continue;
--- tests/PHPStan/Rules/PhpDoc/IncompatiblePropertyPhpDocTypeRuleTest.php
+++ tests/PHPStan/Rules/PhpDoc/IncompatiblePropertyPhpDocTypeRuleTest.php
@@ -59,6 +59,14 @@ class IncompatiblePropertyPhpDocTypeRuleTest extends RuleTestCase
 				'PHPDoc tag @var for property InvalidPhpDoc\FooWithProperty::$unknownClassConstant2 contains unresolvable type.',
 				45,
 			],
+			[
+				'Type projection covariant int in generic type InvalidPhpDocDefinitions\FooCovariantGeneric<covariant int> in PHPDoc tag @var for property InvalidPhpDoc\FooWithProperty::$genericRedundantTypeProjection is redundant, template type T of class InvalidPhpDocDefinitions\FooCovariantGeneric has the same variance.',
+				51,
+			],
+			[
+				'Type projection contravariant int in generic type InvalidPhpDocDefinitions\FooCovariantGeneric<contravariant int> in PHPDoc tag @var for property InvalidPhpDoc\FooWithProperty::$genericIncompatibleTypeProjection is conflicting with variance of template type T of class InvalidPhpDocDefinitions\FooCovariantGeneric.',
+				57,
+			],
 		]);
 	}
 
